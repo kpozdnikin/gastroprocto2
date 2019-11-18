46cf64d77608cf792756dbe753e2c1d3e89c400e
kpozdnikin <kpozdnikin@usetech.ru>
2019-11-18 09:48:05 +0300

    next js

--- src/components/stomachache.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Nav from './nav';
-export default class Stomachache extends React.Component{
-    render(){
-        return <div>
-            <Nav/>
-        </div>;
-    }
-}
