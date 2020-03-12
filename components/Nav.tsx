d477a0b3a6c13c9a7f9898e80a335934206a3e23
kpozdnikin <kpozdnikin@usetech.ru>
2020-03-12 18:24:51 +0300

    add class to link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import NavState from './NavState';
 import MainMenu from './MainMenu';
 
 const Nav = () => {
+  const router = useRouter();
+
+  function getClassName(url: string) {
+    let className = 'topNav';
+    if (router.pathname === url) {
+      className = `topNav selected`
+    }
+    return className;
+  }
+
   return (
     <div className='nav'>
       <strong className='label-top'>
@@ -12,10 +23,10 @@ const Nav = () => {
           <MainMenu />
         </NavState>
       </strong>
-      <Link href="/"><a className='topNav'>Главная</a></Link>
-      <Link href="/blog"><a className='topNav'>Мой блог</a></Link>
-      <Link href="/stomachache"><a className='topNav'>О боли в животе</a></Link>
-      <Link href="/illnesses"><a className='topNav'>Заболевания ЖКТ</a></Link>
+      <Link href="/"><a className={getClassName('/')}>Главная</a></Link>
+      <Link href="/blog"><a className={getClassName('/blog')}>Мой блог</a></Link>
+      <Link href="/stomachache"><a className={getClassName('/stomachache')}>О боли в животе</a></Link>
+      <Link href="/illnesses"><a className={getClassName('/illnesses')}>Заболевания ЖКТ</a></Link>
       <strong className='label-top'>+7 921 726 74 51</strong>
     </div>
   );
