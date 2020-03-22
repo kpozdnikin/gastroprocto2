02a3b6ec10b83682ef1952a09e5049dfb5b53963
sergey87 <tihero@yandex.ru>
2020-03-22 23:26:26 +0300

    add animation for buttons pages illnesses and stomachache

--- pages/illnesses.tsx
+++ pages/illnesses.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Footer from '../components/Footer';
 import Fgds from '../assets/img/fgds.png';
@@ -11,6 +11,14 @@ import Nav from "../components/Nav"
 
 
 const Illnesses = () => {
+
+    const [active, setActive] = useState("");
+    const [active2, setActive2] = useState("");
+    const [active3, setActive3] = useState("");
+    const [active4, setActive4] = useState("");
+    const [active5, setActive5] = useState("");
+    const [active6, setActive6] = useState("");
+    const [active7, setActive7] = useState("");
 
         return (
             <Layout title='Болит живот. Заболевания ЖКТ'>
@@ -52,32 +60,32 @@ const Illnesses = () => {
             <div className='ill_text'>
                 <img src={Organs} alt='Внутренние органы'/>
                 <div className='illnesses_list'>
-                    <button className='collapsible'>Гастрит</button>
-                    <div className='content show'>
+                    <button className='collapsible' onClick={() => {setActive(active===""?"show":"")}}>Гастрит</button>
+                    <div className={`content ${active}`}>
                         <p>Гастрит - это воспаление слизистой оболочки желудка. Острое воспаление слизистой оболочки желудка, острый гастрит, возникает в следствие непродолжительного действия раздражителей: химических веществ, некоторых лекарственных препаратов, недоброкачественной пищи и др. Если слизистая воспаляется неоднократно, то происходит ее структурная перестройка или атрофия – развивается хронический гастрит. При гастрите, как остром, так и хроническом, болевые ощущения локализованы, преимущественно, в верхних отделах живота («под ложечкой»). Интенсивность болей при остром гастрите может быть выраженной, характерным признаком является возникновение или усиление болей после приема пищи. При хроническом гастрите пациент характеризует болевые ощущения как чувство тяжести, дискомфорт, «ноет», «давит». Сопровождаются они изжогой, реже отрыжкой, иногда пациента «мутит». Тошнота и рвота указывает на присоединение вторичного раздражения поджелудочной железы. Диагностика как острого, так и хронического гастрита осуществляется на основании совокупности жалоб пациента и данных эндоскопического исследования - фиброгастроскопии (ФГДС). Данное исследование позволяет осмотреть слизистую оболочку, выявить воспаление, уточнить : какие отделы желудка поражены, провести забор материала (биопсию) для гистологического подтверждения диагноза и тестирования на наличие микроорганизмов «Хеликобактер пилори», вызывающих и поддерживающих воспаление. Знание данного вопроса поможет Вашему лечащему врачу оценить необходимость назначения антибиотиков. Кроме того, при хроническом гастрите нарушается продукция оболочкой желудка химических веществ и гормонов. Наибольшее внимание уделяется нарушению кислотопродуцирующей функции. Для терапевта, назначающего лечение, необходимо знать : повышена кислотность желудочного содержимого или понижена, т.к. лекарственные препараты в этих случаях назначаются диаметрально противоположного действия. Такую информацию можно получить при проведении ФГДС с оценкой кислотопродуцирующей функции.</p>
                     </div>
-                    <button className='collapsible'>Язва</button>
-                    <div className='content'>
+                    <button className='collapsible' onClick={() => {setActive2(active2===""?"show":"")}}>Язва</button>
+                    <div className={`content ${active2}`}>
                         <p>Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва Язва </p>
                     </div>
-                    <button className='collapsible'>Холецистит</button>
-                    <div className='content'>
+                    <button className='collapsible' onClick={() => {setActive3(active3===""?"show":"")}}>Холецистит</button>
+                    <div className={`content ${active3}`}>
                         <p>Холецистит Холецистит Холецистит Холецистит Холецистит Холецистит Холецистит Холецистит Холецистит Холецистит Холецистит Холецистит Холецистит Холецистит Холецистит </p>
                     </div>
-                    <button className='collapsible'>Панкреатит</button>
-                    <div className='content'>
+                    <button className='collapsible' onClick={() => {setActive4(active4===""?"show":"")}}>Панкреатит</button>
+                    <div className={`content ${active4}`}>
                         <p>ПанкреатитПанкреатитПанкреатитПанкреатитПанкреатитПанкреатитПанкреатитПанкреатит</p>
                     </div>
-                    <button className='collapsible'>Гепатит</button>
-                    <div className='content'>
+                    <button className='collapsible' onClick={() => {setActive5(active5===""?"show":"")}}>Гепатит</button>
+                    <div className={`content ${active5}`}>
                         <p>ГепатитГепатитГепатитГепатитГепатитГепатит  ГепатитГепатитГепатитГепатитГепатит</p>
                     </div>
-                    <button className='collapsible'>Колит</button>
-                    <div className='content'>
+                    <button className='collapsible' onClick={() => {setActive6(active6===""?"show":"")}}>Колит</button>
+                    <div className={`content ${active6}`}>
                         <p>КолитКолитКолитКолитКолитКолит Колит super Колит  Колит Колит  Колит Колит  Колит Колит </p>
                     </div>
-                    <button className='collapsible'>Синдром раздраженной кишки (СРК)</button>
-                    <div className='content'>
+                    <button className='collapsible' onClick={() => {setActive7(active7===""?"show":"")}}>Синдром раздраженной кишки (СРК)</button>
+                    <div className={`content ${active7}`}>
                         <p> Синдром раздраженной кишки (СРК)v  Синдром раздраженной кишки (СРК)Синдром раздраженной кишки (СРК)  Синдром раздраженной кишки (СРК)   </p>
                     </div>
                 </div>
--- pages/stomachache.tsx
+++ pages/stomachache.tsx
@@ -17,6 +17,23 @@ const Stomachache = () => {
     const [active5, setActive5] = useState("");
     const [active6, setActive6] = useState("");
 
+    function openClose(num:number){
+        switch (num){
+            case 1: setActive(active===""?"show":"");
+                break;
+            case 2: setActive2(active2===""?"show":"");
+                break;
+            case 3: setActive3(active3===""?"show":"");
+                break;
+            case 4: setActive4(active4===""?"show":"");
+                break;
+            case 5: setActive5(active5===""?"show":"");
+                break;
+            case 6: setActive6(active6===""?"show":"");
+                break;
+        }
+    }
+
     return (
             <Layout title='Болит живот. О боли в животе'>
             <div className='stomachache'>
@@ -54,7 +71,7 @@ const Stomachache = () => {
             <div className='text_stomachache'>
                 <BodySvg />
                 <div className='container'>
-                    <button className='collapsible' onClick={()=>setActive(active==="show"?"":"show")}>Болит под ложечкой <br/>(эпигастральная область)</button>
+                    <button className='collapsible' onClick={()=> {openClose(1)}}>Болит под ложечкой <br/>(эпигастральная область)</button>
                     <div className={`content ${active}`} >
                         <p> Боли в животе под ложечкой, ноющего характера, могут характеризоваться пациентом как "дискомфорт", "чувство тяжести", возникающие, после приема пищи, сопровождающиеся изжогой более характерны для хронического гастрита.
                             <br/><br/>При плохой работе пищеводно-желудочного клапана - недостаточности кардии - болевые ощущения могут распространяться на область за грудиной, имитировать сердечный приступ. Неприятным сюрпризом может быть запах изо рта.
@@ -62,27 +79,27 @@ const Stomachache = () => {
                             <br/><br/>ЛЮБАЯ ОСТРАЯ БОЛЬ, ВОЗНИКШАЯ ВПЕРВЫЕ, ТРЕБУЕТ НЕЗАМЕДЛИТЕЛЬНОГО ОБРАЩЕНИЯ К ВРАЧУ ДЛЯ ПОСЛЕДУЮЩЕГО ОБСЛЕДОВАНИЯ</p>
                         <Button text='Записаться' myStyle='button'/>
                     </div>
-                    <button className='collapsible' onClick={()=>setActive2(active2==="show"?"":"show")}>Болит справа вверху живота, ближе к ребрам.<br/>(область правого подреберья)</button>
+                    <button className='collapsible' onClick={()=>{openClose(2)}}>Болит справа вверху живота, ближе к ребрам.<br/>(область правого подреберья)</button>
                     <div className={`content ${active2}`}>
                         <p>Какой-то текст</p>
                         <Button text='Записаться' myStyle='button'/>
                     </div>
-                    <button className='collapsible' onClick={()=>setActive3(active3==="show"?"":"show")}>Болит справа или слева сбоку живота. Колет в боку.<br/> (правая или левая боковая область)</button>
+                    <button className='collapsible' onClick={()=>{openClose(3)}}>Болит справа или слева сбоку живота. Колет в боку.<br/> (правая или левая боковая область)</button>
                     <div className={`content ${active3}`}>
                         <p>Какой-то текст</p>
                         <Button text='Записаться' myStyle='button'/>
                     </div>
-                    <button className='collapsible' onClick={()=>setActive4(active4==="show"?"":"show")}>Болит слева или справа внизу живота<br/> (подвздошная область справа или слева)</button>
+                    <button className='collapsible' onClick={()=>{openClose(4)}}>Болит слева или справа внизу живота<br/> (подвздошная область справа или слева)</button>
                     <div className={`content ${active4}`}>
                         <p>Какой-то текст</p>
                         <Button text='Записаться' myStyle='button'/>
                     </div>
-                    <button className='collapsible' onClick={()=>setActive5(active5==="show"?"":"show")}>Болит низ живота по середине<br/>(надлобковая область)</button>
+                    <button className='collapsible' onClick={()=>{openClose(5)}}>Болит низ живота по середине<br/>(надлобковая область)</button>
                     <div className={`content ${active5}`}>
                         <p>Какой-то текст</p>
                         <Button text='Записаться' myStyle='button'/>
                     </div>
-                    <button className='collapsible' onClick={()=>setActive6(active6==="show"?"":"show")}>Болит в середине живота<br/>(околопупочная область)</button>
+                    <button className='collapsible' onClick={()=>{openClose(6)}}>Болит в середине живота<br/>(околопупочная область)</button>
                     <div className={`content ${active6}`}>
                         <p>Какой-то текст</p>
                         <Button text='Записаться' myStyle='button'/>
