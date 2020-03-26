cc38619b925bb45c5828762d290d3ad07c8870b4
kpozdnikin <kpozdnikin@usetech.ru>
2020-03-26 15:38:15 +0300

    delete some figures from background

--- pages/illnesses.tsx
+++ pages/illnesses.tsx
@@ -4,10 +4,10 @@ import Footer from '../components/Footer';
 import Fgds from '../assets/img/fgds.png';
 import Rrc from '../assets/img/rrs.png';
 import Fks from '../assets/img/fks.png';
-import Button from "../components/Button";
+import Button from '../components/Button';
 import Organs from '../assets/img/organs.svg';
-import Layout from "../components/Layout";
-import Nav from "../components/Nav"
+import Layout from '../components/Layout';
+import Nav from '../components/Nav';
 
 
 const Illnesses = () => {
@@ -21,36 +21,31 @@ const Illnesses = () => {
     const [active7, setActive7] = useState("");
 
         return (
-            <Layout title='Болит живот. Заболевания ЖКТ'>
-            <div className='illnesses'>
+            <Layout title="Болит живот. Заболевания ЖКТ">
+            <div className="illnesses">
                 <Nav/>
-            <div className='top_ill'>
+            <div className="top_ill">
 
                 <h1>Заболевания желудочно-кишечного тракта</h1>
                 <div className='rec20'/>
-                <p>Эндоскопические методы исследования, позволяющие осмотреть пищевод, желудок, кишечник изнутри, со стороны слизистой оболочки.</p>
+                <p>Эндоскопические методы исследования,
+                    позволяющие осмотреть пищевод, желудок, кишечник изнутри, со стороны слизистой оболочки.</p>
             </div>
             <div className='inspection'>
                 <div className='fgds'>
-                    <div className='rec20'/>
-                    <div className='rec21'/>
                     <img src={Fgds} alt='ФГДС'/>
                     <h2>фгдс</h2>
                     <p>Осмотр пищевода, желудка и начального отдела (луковицы) двенадцатиперстной кишки</p>
                     <Button text='записаться' myStyle='button'/>
                 </div>
                 <div className='rrs'>
-                    <div className='rec22'/>
-                    <div className='rec23'/>
                     <img src={Rrc} alt='РРС'/>
                     <h2>ррс</h2>
                     <p>Осмотр прямой кишки и анального канала с помощью тонкой трубки с оптическим волокном.</p>
                     <Button text='записаться' myStyle='button'/>
                 </div>
                 <div className='fks'>
-                    <div className='rec25'/>
                     <img src={Fks} alt='ФКС'/>
-                    <div className='rec24'/>
                     <h2>фкс</h2>
                     <p>Осмотр толстой кишки с помощью длинной трубки с оптическим волокном.</p>
                     <Button text='записаться' myStyle='button'/>
